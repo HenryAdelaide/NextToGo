584371c598d837b8bd8eb636b60855fd77804b60
Henry Liu <henry1995.liu@gmail.com>
2024-11-04 08:57:16 +1030

    update Documentation and gitignore

--- NextToGo/Extensions/UIView+Extension.swift
+++ NextToGo/Extensions/UIView+Extension.swift
@@ -8,12 +8,18 @@
 import Foundation
 import UIKit
 
+/// Extension tools which can improve simplicity and reduce dumplicate work
 extension UIView {
     
+    /// Associated Bundle
     public static var associatedBundle: Bundle {
         Bundle(for: self.classForCoder())
     }
     
+    
+    /// Load Nib/Xib from Bundle into UIView
+    /// - Parameter frame: frame you need to use
+    /// - Returns: Loaded UIView from Nib/Xib
     public static func loadNibView(frame: CGRect? = nil) -> Self {
         let view = associatedBundle.loadNibNamed(String(describing: self), owner: nil, options: nil)?.first as! Self
         if let frame {
@@ -22,11 +28,16 @@ extension UIView {
         return view
     }
     
+    /// Convenience to create UINib
     public static var loadNib: UINib {
         let nib = UINib(nibName: String(describing: self), bundle: associatedBundle)
         return nib
     }
     
+    /**
+        Identifier when register
+        e.g UITableView.register
+     */
     public static var reuseId: String {
         return String(describing: self)
     }
